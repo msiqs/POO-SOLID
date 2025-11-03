de7f3b1882c2b16c4421e4d01176c83f947f35a0
Rafael Machado <rafaelmachadocd37@gmail.com>
2025-11-03 12:00:57 -0300

    State Pattern inicializado

--- resolução.ts
+++ resolução.ts
@@ -1,36 +1,10 @@
-interface state{
-  online: boolean
-  
-  turnOn():void
-  turnOff():void
-  onGame():void
+interface IJogadorState{
+  turnOn(context: Jogador):void
+  turnOff(context: Jogador):void
+  onGame(context: Jogador):void
+  pause(context: Jogador):void
+  resumeGame(context: Jogador):void
+  disconnect(context: Jogador):void
 }
 
-interface inGame{
-  pause():void
-  disconnect():void
-}
-
-interface pausado{
-  resumeGame():void
-  disconnect():void
-}
-
-class jogadorState implements state{
-  online: boolean
-  
-  constructor(online: boolean){
-    this.online = false
-  }
-  turnOn():void{
-    if(this.online = false){
-      this.online = true
-    } else {
-      console.log(`Você já está online`)
-    }
-  }
-  turnOff():void{
-
-  }
-  onGame():void
-}
+class Jogador{}
